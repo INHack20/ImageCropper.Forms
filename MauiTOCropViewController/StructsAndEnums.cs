44581723ab55b4c9c4f1ceed78f4968baa1d611f
Carlos Mendoza <inhack20@gmail.com>
2024-08-05 23:14:16 -0400

    lib ios

--- MauiTOCropViewController/StructsAndEnums.cs
+++ MauiTOCropViewController/StructsAndEnums.cs
@@ -1,4 +1,27 @@
 namespace MauiTOCropViewController
 {
+    public enum TOCropViewCroppingStyle : ulong
+    {
+        Default,
+        Circular
+    }
 
+    public enum TOCropViewControllerAspectRatioPreset : ulong
+    {
+        Original,
+        Square,
+        TOCropViewControllerAspectRatioPreset3x2,
+        TOCropViewControllerAspectRatioPreset5x3,
+        TOCropViewControllerAspectRatioPreset4x3,
+        TOCropViewControllerAspectRatioPreset5x4,
+        TOCropViewControllerAspectRatioPreset7x5,
+        TOCropViewControllerAspectRatioPreset16x9,
+        Custom
+    }
+
+    public enum TOCropViewControllerToolbarPosition : ulong
+    {
+        Bottom,
+        Top
+    }
 }
