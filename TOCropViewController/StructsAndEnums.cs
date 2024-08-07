082131fcdff3348e584158dc6dd988bbebc132ac
Carlos Mendoza <inhack20@gmail.com>
2024-08-07 13:59:57 -0400

    upgrade lib

--- TOCropViewController/StructsAndEnums.cs
+++ TOCropViewController/StructsAndEnums.cs
@@ -1,33 +1,43 @@
-using System;
+using System.Runtime.InteropServices;
+using Foundation;
 using ObjCRuntime;
 
 namespace Bind_TOCropViewController
 {
     [Native]
-    public enum TOCropViewCroppingStyle : ulong
-    {
-        Default,
-        Circular
-    }
+public enum TOCropViewCroppingStyle : long
+{
+	Default,
+	Circular
+}
 
-    [Native]
-    public enum TOCropViewControllerAspectRatioPreset : ulong
-    {
-        Original,
-        Square,
-        TOCropViewControllerAspectRatioPreset3x2,
-        TOCropViewControllerAspectRatioPreset5x3,
-        TOCropViewControllerAspectRatioPreset4x3,
-        TOCropViewControllerAspectRatioPreset5x4,
-        TOCropViewControllerAspectRatioPreset7x5,
-        TOCropViewControllerAspectRatioPreset16x9,
-        Custom
-    }
+[Native]
+public enum TOCropViewControllerAspectRatioPreset : long
+{
+	Original,
+	Square,
+	TOCropViewControllerAspectRatioPreset3x2,
+	TOCropViewControllerAspectRatioPreset5x3,
+	TOCropViewControllerAspectRatioPreset4x3,
+	TOCropViewControllerAspectRatioPreset5x4,
+	TOCropViewControllerAspectRatioPreset7x5,
+	TOCropViewControllerAspectRatioPreset16x9,
+	Custom
+}
 
-    [Native]
-    public enum TOCropViewControllerToolbarPosition : ulong
-    {
-        Bottom,
-        Top
-    }
+[Native]
+public enum TOCropViewControllerToolbarPosition : long
+{
+	Bottom,
+	Top
+}
+
+static class CFunctions
+{
+	// NSBundle * TO_CROP_VIEW_RESOURCE_BUNDLE_FOR_OBJECT (NSObject *object);
+	[DllImport ("__Internal")]
+	[Verify (PlatformInvoke)]
+	static extern NSBundle TO_CROP_VIEW_RESOURCE_BUNDLE_FOR_OBJECT (NSObject @object);
+}
+
 }
--- TOCropViewController/TOCropViewController.framework/Headers/TOCropToolbar.h
+++ TOCropViewController/TOCropViewController.framework/Headers/TOCropToolbar.h
@@ -1,7 +1,7 @@
 //
 //  TOCropToolbar.h
 //
-//  Copyright 2015-2018 Timothy Oliver. All rights reserved.
+//  Copyright 2015-2024 Timothy Oliver. All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to
@@ -22,6 +22,14 @@
 
 #import <UIKit/UIKit.h>
 
+#if !__has_include(<TOCropViewController/TOCropViewConstants.h>)
+#import "TOCropViewConstants.h"
+#else
+#import <TOCropViewController/TOCropViewConstants.h>
+#endif
+
+NS_ASSUME_NONNULL_BEGIN
+
 @interface TOCropToolbar : UIView
 
 /* In horizontal mode, offsets all of the buttons vertically by height of status bar. */
@@ -32,24 +40,28 @@
 
 /* The 'Done' buttons to commit the crop. The text button is displayed
  in portrait mode and the icon one, in landscape. */
-@property (nonnull, nonatomic, strong, readonly) UIButton *doneTextButton;
-@property (nonnull, nonatomic, strong, readonly) UIButton *doneIconButton;
-@property (nonnull, nonatomic, copy) NSString *doneTextButtonTitle;
-
+@property (nonatomic, strong, readonly) UIButton *doneTextButton;
+@property (nonatomic, strong, readonly) UIButton *doneIconButton;
+@property (nonatomic, copy) NSString *doneTextButtonTitle;
+@property (null_resettable, nonatomic, copy) UIColor *doneButtonColor;
 
 /* The 'Cancel' buttons to cancel the crop. The text button is displayed
  in portrait mode and the icon one, in landscape. */
-@property (nonnull, nonatomic, strong, readonly) UIButton *cancelTextButton;
-@property (nonnull, nonatomic, strong, readonly) UIButton *cancelIconButton;
-@property (nonnull, nonatomic, copy) NSString *cancelTextButtonTitle;
+@property (nonatomic, strong, readonly) UIButton *cancelTextButton;
+@property (nonatomic, strong, readonly) UIButton *cancelIconButton;
+@property (nonatomic, readonly) UIView *visibleCancelButton;
+@property (nonatomic, copy) NSString *cancelTextButtonTitle;
+@property (nullable, nonatomic, copy) UIColor *cancelButtonColor;
+
+@property (nonatomic, assign) BOOL showOnlyIcons;
 
 /* The cropper control buttons */
-@property (nonnull, nonatomic, strong, readonly)  UIButton *rotateCounterclockwiseButton;
-@property (nonnull, nonatomic, strong, readonly)  UIButton *resetButton;
-@property (nonnull, nonatomic, strong, readonly)  UIButton *clampButton;
+@property (nonatomic, strong, readonly)  UIButton *rotateCounterclockwiseButton;
+@property (nonatomic, strong, readonly)  UIButton *resetButton;
+@property (nonatomic, strong, readonly)  UIButton *clampButton;
 @property (nullable, nonatomic, strong, readonly) UIButton *rotateClockwiseButton;
 
-@property (nonnull, nonatomic, readonly) UIButton *rotateButton; // Points to `rotateCounterClockwiseButton`
+@property (nonatomic, readonly) UIButton *rotateButton; // Points to `rotateCounterClockwiseButton`
 
 /* Button feedback handler blocks */
 @property (nullable, nonatomic, copy) void (^cancelButtonTapped)(void);
@@ -67,6 +79,12 @@
 @property (nonatomic, assign) BOOL clampButtonHidden;
 @property (nonatomic, assign) BOOL rotateCounterclockwiseButtonHidden;
 @property (nonatomic, assign) BOOL rotateClockwiseButtonHidden;
+@property (nonatomic, assign) BOOL resetButtonHidden;
+@property (nonatomic, assign) BOOL doneButtonHidden;
+@property (nonatomic, assign) BOOL cancelButtonHidden;
+
+/* For languages like Arabic where they natively present content flipped from English */
+@property (nonatomic, assign) BOOL reverseContentLayout;
 
 /* Enable the reset button */
 @property (nonatomic, assign) BOOL resetButtonEnabled;
@@ -74,5 +92,6 @@
 /* Done button frame for popover controllers */
 @property (nonatomic, readonly) CGRect doneButtonFrame;
 
-
 @end
+
+NS_ASSUME_NONNULL_END
